c7f2cb06ee97ada093e1a1662534d9a971f0a9db
Paschal Ramdan <dane.ramdan@gmail.com>
2022-05-19 10:09:08 -0400

    commit tree 130df579b398f3792124ef9c4e34c31f0cc02f83

---
+++ app.rb
@@ -0,0 +1,8 @@
+# app.rb
+puts "Hello World!"
+p [1, 2, 3]
+
+# app.rb
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+print ("Pass this test, please.")
